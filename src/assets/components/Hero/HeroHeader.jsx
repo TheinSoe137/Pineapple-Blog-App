d49e9fcb3dcb8aa9f53f9b7ed8efd3a1676bc34a
PeterSol <thein4edu@gmail.com>
2024-03-02 23:39:54 +0630

    adding heading for demo acc

--- src/assets/components/Hero/HeroHeader.jsx
+++ src/assets/components/Hero/HeroHeader.jsx
@@ -4,8 +4,9 @@ function HeroHeader() {
     <div className="hero-header bg-blur bg-top">
       <h1>Pineapple</h1>
       <p className="text-regular">
-        Your daily dose of sunshine and social connection
+        Your daily dose of sunshine and social connection.
       </p>
+      <span>You can directly Login through Demo Account.</span>
     </div>
   );
 }
